756d69a642562950daca0f43de0c6e7f6ec8c879
Matt Bates <batesmatthewj@gmail.com>
2023-07-22 15:34:29 -0500

    trailer animation

--- Assets/Scripts/HitchMeUp.cs
+++ Assets/Scripts/HitchMeUp.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HitchMeUp : MonoBehaviour
 {
     // Start is called before the first frame update
+    public RoverController roverController;
     void Start()
     {
         
@@ -13,19 +14,30 @@ public class HitchMeUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (roverController) {
+            transform.parent.position = roverController.model.position - roverController.model.forward * .18f;
+            var backPos = transform.position - transform.forward * .5f;
+            Debug.DrawRay(backPos + Vector3.up*.5f, Vector3.down, Color.blue, .01f);
+            if (Physics.Raycast(backPos + Vector3.up*.5f, Vector3.down, out var hit, 1f, Physics.AllLayers)) {
+                Debug.DrawRay(hit.point, -Vector3.forward, Color.red, .01f);
+                var delta = (transform.position - Vector3.up*.25f - hit.point).normalized;
+                float angleDegrees = Mathf.Asin(-delta.y) * 180 / Mathf.PI;
+                transform.parent.localEulerAngles = new Vector3(angleDegrees, -.5f * roverController.animatedWheelDir, 0);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other){
         Debug.Log("This probably will not print");
         Debug.Log("ok in case this prints, the object that triggers is - " + other.gameObject.name);
-        if(other.gameObject.name == "TowHitch"){
+        if(other.gameObject.name == "TowHitch") {
             Debug.Log("aight bet hitch me up");
             HitchMeUpDaddy(other.transform.parent);
         }
     }
 
     void HitchMeUpDaddy(Transform rover){
+        roverController = rover.GetComponent<RoverController>();
         transform.parent.parent = rover;
         transform.parent.localPosition = new Vector3(0f, 0.12f, -.22f);
         transform.parent.rotation = transform.parent.parent.rotation;
