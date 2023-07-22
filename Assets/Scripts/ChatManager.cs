d0e0969a6cea2a01487c946ffad8d1798afb67ef
Jacob Kreiner <kreinerjm@hotmail.com>
2023-07-22 17:23:39 -0400

    Scrolling text UI and sound

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChatManager : MonoBehaviour
+{
+    string targetText = "";
+    float writeSpeed = 0.05f;
+    float currentTime = 0f;
+    bool writing = false;
+    public AudioSource morseCode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(writing){
+            currentTime += Time.deltaTime;
+            int numChars = (int)(currentTime/writeSpeed);
+
+            if(Input.GetKeyUp(KeyCode.Space)){
+                numChars = targetText.Length + 1;
+                GetComponentInChildren<TextMeshProUGUI>().text = targetText;
+            }
+
+            if(numChars > targetText.Length){
+                writing = false;
+                morseCode.Stop();
+            } else {
+                GetComponentInChildren<TextMeshProUGUI>().text = targetText.Substring(0,numChars);
+            }
+        } else {
+            if(Input.GetKeyUp(KeyCode.Space)){
+                Hide();
+            }
+        }
+    }
+
+    public void ShowAndStartText(string targetText){
+        currentTime = 0f;
+        this.targetText = targetText;
+        GetComponentInChildren<TextMeshProUGUI>().text = "";
+        writing = true;
+        gameObject.SetActive(true);
+        morseCode.Play();
+    }
+
+    void Hide(){
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -11,18 +11,23 @@ public class PauseMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponentInChildren<Canvas>(true).gameObject.SetActive(false);
+        GameObject canvasObject = gameObject.GetComponentInChildren<Canvas>(true).gameObject;
+        //assumes that the pause menu is the first child of the canvas
+        canvasObject.transform.GetChild(0).gameObject.SetActive(false);
         unstuckButton.onClick.AddListener(delegate { Unstuck(); });
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject canvasObject = gameObject.GetComponentInChildren<Canvas>(true).gameObject;
         if(Input.GetKeyUp(KeyCode.Escape)){
-            GameObject canvasObject = gameObject.GetComponentInChildren<Canvas>(true).gameObject;
-            canvasObject.SetActive(!active);
+            //assumes that the pause menu is the first child of the canvas
+            canvasObject.transform.GetChild(0).gameObject.SetActive(!active);
             active = !active;
-        }        
+        } else if(Input.GetKeyUp(KeyCode.P)){
+            GetComponentInChildren<ChatManager>(true).ShowAndStartText("This message should be written out over a few seconds \n \nDo new lines work?\n\n\nmaking this extra long to test the skipping functionality lol idk what to type here matt and chandler are binks im a ui god please work on first try gg ez no re");
+        } 
     }
 
     void Unstuck(){
