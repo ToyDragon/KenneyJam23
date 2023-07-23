c841a8844e4a6c24fb2d5cbc9c813608c9019308
Jacob Kreiner <kreinerjm@hotmail.com>
2023-07-22 22:23:14 -0400

    Mission objective, controls UI, and initial dialogs

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -17,8 +17,11 @@ public class ChatManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    void OnEnable(){
         instance = this;
-        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/MissionObjectiveManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MissionObjectiveManager : MonoBehaviour
+{
+    public static MissionObjectiveManager instance;
+    int numObjectives = 5;
+    int currentObjective = -1;
+
+    string[] objectives = new string[]{
+        "Locate the ROV-ER Multi-Tool",
+        "Escape the crater",
+        "Locate and hitch the trailer",
+        "Gather some Marsium"
+    };
+
+    string[] dialogs = new string[]{
+        "Come in ROV-ER! This is captain Buzz of the Mars Exploration Task Force.\n\n"
+        +"Our crew is out of supplies and we are currently experiencing a comms outage with systems command.\n\n"
+        +"You need to reactivate the communication towers spread around the field operating base.\n\n"
+        +"To get over there you will need to collect your ROV-ER Multi-Tool to help traverse the terrain.\n\n"
+        +"Quickly please, I know not how much longer we will last."
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        CompleteCurrentObjective();
+    }
+
+    void OnEnable(){
+        instance = this;
+    }
+
+    public void SetObjective(string objective){
+        GetComponent<TextMeshProUGUI>().text = objective;
+    }
+
+    public void CompleteCurrentObjective(){
+        currentObjective++;
+        if(currentObjective <= numObjectives){
+            ChatManager.instance.ShowAndStartText(dialogs[0]);
+            SetObjective(objectives[currentObjective]);
+        }
+    }
+}
